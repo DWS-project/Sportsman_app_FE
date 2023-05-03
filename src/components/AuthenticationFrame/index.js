72ae483db151813067d33b13250cd753a606e3a4
berinaspirjan <berina.spirjan@treca-gimnazija.edu.ba>
2023-05-03 21:14:52 +0200

    Implemented form for registration and connected frontend and backend

---
+++ src/components/AuthenticationFrame/index.js
@@ -0,0 +1,39 @@
+import useStyles from './styles'
+import Avatar from '@mui/material/Avatar'
+import CssBaseline from '@mui/material/CssBaseline'
+import Paper from '@mui/material/Paper'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+// import LoginForm from '../Login'
+import RegistrationForm from '../Registration'
+
+const AuthenticationFrame = () => {
+  const classes = useStyles()
+
+  return (
+    <Grid container component="main" sx={{ height: '100vh' }}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.rightContainer} />
+      <Grid
+        item
+        xs={12}
+        sm={8}
+        md={5}
+        component={Paper}
+        elevation={6}
+        square
+        className={classes.leftContainer}
+      >
+        <Box sx={{ my: 8, mx: 4 }} className={classes.leftContainer}>
+          <Avatar sx={{ m: 1, bgcolor: '#43bbbf' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <RegistrationForm />
+        </Box>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default AuthenticationFrame

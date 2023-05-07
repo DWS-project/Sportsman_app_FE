7551b953c9da89e8cd4042a31abb9c9ea45ff844
Hamza Delic <delic7262@gmail.com>
2023-05-07 17:55:01 +0200

    Updated registration for users, added fields age, city and interests

--- src/components/AuthenticationFrame/index.js
+++ src/components/AuthenticationFrame/index.js
@@ -5,6 +5,7 @@ import Paper from '@mui/material/Paper'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import Typography from '@mui/material/Typography'
 // import LoginForm from '../Login'
 import RegistrationForm from '../Registration'
 
@@ -29,6 +30,9 @@ const AuthenticationFrame = () => {
           <Avatar sx={{ m: 1, bgcolor: '#43bbbf' }}>
             <LockOutlinedIcon />
           </Avatar>
+          <Typography component="h1" variant="h5" sx={{mb: 5}}>
+            Registrujte se
+          </Typography>
           <RegistrationForm />
         </Box>
       </Grid>

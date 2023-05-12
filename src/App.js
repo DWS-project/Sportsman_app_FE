fbdf2b2d852475ed6aa1ae537053db749d0f1d13
berinaspirjan <berina.spirjan@treca-gimnazija.edu.ba>
2023-05-13 00:03:28 +0200

    Updated configuration for prettier

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css'
 import AuthenticationFrame from './components/AuthenticationFrame'
 
 function App() {
+  console.log('process.env.FRONTEND_URL', process.env.FRONTEND_URL)
+
   return (
     <div>
       <AuthenticationFrame />

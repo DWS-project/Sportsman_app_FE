64fb66c15d6f0726d0eda540fc3ee9d91a2be6b5
berinaspirjan <berina.spirjan@treca-gimnazija.edu.ba>
2023-05-13 00:03:55 +0200

    Removed console.log

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css'
 import AuthenticationFrame from './components/AuthenticationFrame'
 
 function App() {
-  console.log('process.env.FRONTEND_URL', process.env.FRONTEND_URL)
-
   return (
     <div>
       <AuthenticationFrame />

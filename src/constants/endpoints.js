bf2d83f30c2eea367b696e395421915e0100fbcf
berinaspirjan <berina.spirjan@treca-gimnazija.edu.ba>
2023-05-20 19:25:08 +0200

    Fixed endpoints

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,6 +1,6 @@
 export const BASE_BACKEND_URL = process.env.REACT_APP_BACKEND_URL
 export const AUTHENTICATION = `${BASE_BACKEND_URL}/authentication`
-export const LOGIN = `${AUTHENTICATION}/login/`
+export const LOGIN = `${AUTHENTICATION}/login`
 export const REGISTRATION_PLAYER = `${AUTHENTICATION}/register-player`
-export const REGISTRATION_OWNER = `${AUTHENTICATION}/register-owner/`
+export const REGISTRATION_OWNER = `${AUTHENTICATION}/register-owner`
 export const LOGOUT = `${AUTHENTICATION}/logout`

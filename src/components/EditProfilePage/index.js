b2ace05857f1cb595af5773853ce2323ad06d63b
berinaspirjan <berina.spirjan@treca-gimnazija.edu.ba>
2023-06-18 14:10:57 +0200

    Fixed problem with sorting imports

--- src/components/EditProfilePage/index.js
+++ src/components/EditProfilePage/index.js
@@ -2,6 +2,7 @@ import Cookies from 'js-cookie'
 import React from 'react'
 import { COOKIE_AUTHENTICATION_FE } from 'src/constants/keys/browser'
 import withMainFrame from 'src/hoc/withMainFrame'
+
 import { EditUserPage } from './User'
 
 export const EditProfilePage = () => {
